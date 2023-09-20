f2910c5a9c157290fe6103783a8f61e1235c6017
test <test@gmail.com>
2023-09-20 17:31:27 -0400

    this 3rd time saving

---
+++ src/main/java/GITPractice2023/GIT_practice/App.java
@@ -0,0 +1,13 @@
+package GITPractice2023.GIT_practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
